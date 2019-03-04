f97f93aea0cac1790cfd446e812d1101f227ef2b
Unknown <mikelsolla007@gmail.com>
2019-03-04 13:53:37 +0100

    Seguir ejercicio2

--- Tema 9/Ejercicio1_BaseDatos.java
+++ Tema 9/Ejercicio1_BaseDatos.java
@@ -1,13 +1,19 @@
 package ejercicio1_basedatos;
 
 import Ventanas.*;
+import BaseDatos.*;
+import UML.*;
 public class Ejercicio1_BaseDatos {
 
     private static Ventana1 v;
+    private static Conexion CBD;
     
-    public static void main(String[] args) {
-        
+    
+    public static void main(String[] args) throws Exception {
         añadir();
+        CBD = new Conexion();
+        CBD.conectar();
+        
     }
     
     public static void añadir(){
