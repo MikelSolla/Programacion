026e2b4ffc90f19df62c0e1226061fcab7622f88
Unknown <mikelsolla007@gmail.com>
2019-03-05 09:27:11 +0100

    MySQL

---
+++ Tema 9/acontecimiento.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.4.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 05-03-2019 a las 08:17:48
+-- Versión del servidor: 5.6.13
+-- Versión de PHP: 5.4.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de datos: `ejercicio2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `acontecimiento`
+--
+
+CREATE TABLE IF NOT EXISTS `acontecimiento` (
+  `Nombre` text NOT NULL,
+  `Lugar` text NOT NULL,
+  `Fecha` date NOT NULL,
+  `Hora inicio` time NOT NULL,
+  `Hora finalizar` time NOT NULL,
+  `Asistentes` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
